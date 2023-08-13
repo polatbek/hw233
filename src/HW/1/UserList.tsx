55b174075c3f595250ad7474c712a8e4a236cc68
polatbek <polatturgunov@gmail.com>
2023-08-14 00:20:32 +0600

    hw2Add

--- src/HW/1/UserList.tsx
+++ src/HW/1/UserList.tsx
@@ -20,8 +20,7 @@ export const UserList = (props:UserListPropsType) => {
       <h2>User List:</h2>
 
       <ul>
-        {props.users.map((user) => (
-          <li key={user.id} id={`hw01-user-${user.id}`}>
+        {props.users.map((user) => (<li key={user.id} id={`hw01-user-${user.id}`}>
             <strong>{user.name}</strong> (Age: {user.age})<strong> Address:</strong>
             {user.address.street}, {user.address.city}
           </li>
